f0edce6878503d19e0aa123fa0e39ec4c5ba9293
Larii2024 <larina2467m@gmail.com>
2025-03-04 18:13:53 -0500

    Final deployment error fixes

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
-export { default as About } from './about/about'
 export { default as BackToTop } from './back-to-top/back-to-top'
 export { default as Blog } from './blog/blog'
 export { default as SingleBlog } from './blog/blog-card/blog-card'
