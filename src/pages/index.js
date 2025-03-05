55150b021b54770c98999bbb5cfc3f29a9bab5f0
Larii2024 <larina2467m@gmail.com>
2025-03-04 19:13:18 -0500

    Deployment fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import '../styles/globals.css';
 import { Contacts, Education,
   Experience, Landing, Navbar, Projects, Skills
 } from '../components';
 import BackToTop from '../components/back-to-top/back-to-top';
 import ChangeTheme from '../components/change-theme/change-theme';
+
 
 function HomePage() {
 
